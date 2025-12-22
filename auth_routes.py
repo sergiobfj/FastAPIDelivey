aa99eccfd29ebf9c3cc22b3ca311a7c4c99492de
sergio <sjunior1557@gmail.com>
2025-12-22 15:13:52 -0300

    new: criando usuarios, schemas e criptografia

--- auth_routes.py
+++ auth_routes.py
@@ -1,11 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from models import User
+from dependencies import get_session
+from main import bcrypt_context
+from schemas import UserSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix='/auth', tags=["auth"])
 
 @auth_router.get('/')
-async def auth():
+async def home():
     """
     Essa é a rota padrão de autenticação
     """
     return{"message":"Você está na rota de autenticação", "auth":False}
 
+@auth_router.post("/create")
+async def create_account(user_schema: UserSchema, session: Session=Depends(get_session)):
+    user = session.query(User).filter(User.user_schema.email==user_schema.email).first()
+    if user:
+        raise HTTPException(status_code=400, detail="Usuário já cadastrado.")
+    else:
+        encrypted_password = bcrypt_context.hash(user_schema.password)
+        new_user = User(user_schema.name, user_schema.email, encrypted_password)
+        session.add(new_user)
+        session.commit()
+        return {'message': f'Usuário {user_schema.email} cadastrado com sucesso.'}
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 #run code | uvicorn main:app --reload
 from fastapi import FastAPI
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = FastAPI()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 from auth_routes import auth_router
 from order_routes import order_router
